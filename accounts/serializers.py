0ee350b341b1f6fb5869fa0737586d9f96f39c85
Artur Comoli <artur.comoli@gmail.com>
2022-06-29 18:46:32 -0300

    feat: registration and login for accounts done

---
+++ accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = [
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "is_seller",
+            "date_joined",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "date_joined": {"read_only": True},
+        }
+
+    def create(self, validated_data: dict):
+        return Account.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics, views
+from rest_framework.authentication import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.views import Response, status
 
-# Create your views here.
+from .models import Account
+from .serializers import AccountSerializer, LoginSerializer
+
+
+class AccountView(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class ListAccountsByGivenNum(generics.ListAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_queryset(self):
+        max_accounts = self.kwargs["num"]
+        return self.queryset.order_by("-date_joined")[0:max_accounts]
+
+
+class LoginView(views.APIView):
+    def post(self, request):
+
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = authenticate(
+            username=serializer.validated_data["email"],
+            password=serializer.validated_data["password"],
+        )
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key})
+
+        return Response(
+            {"detail": "invalid email or password"},
+            status.HTTP_401_UNAUTHORIZED,
+        )
