3b05e1d325fcaa7f0e0a194404c870871dde9592
Artur Comoli <artur.comoli@gmail.com>
2022-06-30 15:13:43 -0300

    feat: products app created, migrated and permissions and authentication implemented for products views

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,8 @@ from accounts.utils import CustomUserManager
 # Create your models here.
 class Account(AbstractUser):
     username = None
+    last_login = None
+    is_staff = None
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
